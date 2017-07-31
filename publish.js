b8e1a45d20694ea4a6937799317b563a418cbbc6
Jay Chung <xjkit54780@gmail.com>
2017-07-31 12:53:35 +0800

    Update prepublish

--- publish.js
+++ publish.js
@@ -1,11 +1,15 @@
 const ghpages = require('gh-pages');
+
+const repoBranch = 'master';
+const repoUrl = 'https://github.com/joey54780/joey54780.github.io.git';
+
 ghpages.publish('dist', {
-  branch: 'master',
-  repo: 'https://github.com/joey54780/joey54780.git',
+  branch: repoBranch,
+  repo: repoUrl,
 }, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('push to gh-pages successfully');
+    console.log(`push to: ${repoUrl} on branch ${repoBranch} successfully.`);
   }
 });
